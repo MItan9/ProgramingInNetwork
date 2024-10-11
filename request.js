24be9cf76b7786a85a063b8b66a0c9d781adfbe1
Mihailova Tatiana <tatiana.mihailova@isa.utm.md>
2024-10-11 14:24:09 +0300

    extract additional info from the link

--- request.js
+++ request.js
@@ -1,6 +1,10 @@
 const targetUrl = 'https://www.salamander.md/ro';
 const proxyUrl = 'http://localhost:8080/';
 
+
+                                    //  FIRST TASK
+
+
 fetch(proxyUrl + targetUrl)
   .then(response => {
     if (!response.ok) {
@@ -16,6 +20,9 @@ fetch(proxyUrl + targetUrl)
 
     const doc = parser.parseFromString(html, 'text/html');
 
+
+
+                                    //  SECOND TASK
     const products = [];
 
     const productElements = doc.querySelectorAll('.product-card'); 
@@ -30,13 +37,57 @@ fetch(proxyUrl + targetUrl)
       const priceElement = element.querySelector('.text-accent');
       const price = priceElement ? priceElement.innerText.trim() : 'Price doesnt exist'; 
 
-      if (name && price) {
-        products.push({ name, price });
+      console.log(typeof price);
+
+      const linkElement = element.querySelector('a.card-img-top');
+      let link = linkElement ? linkElement.href : null; 
+  
+
+      if (link) {
+        link = link.replace(/^file:\/\/\/C:[^/]*/, ''); 
+        link = link.replace(/\/ro/, ''); 
+        products.push({ name, price, link }); 
       }
+
+
     });
 
     // console.log(products);
+
+                                      //  THIRD TASK
+     const maxRequests = Math.min(products.length, 10); 
+    for (let i = 0; i < maxRequests; i++) {
+      scrapeProductData(products[i].link); 
+    }
+
+
   })
   .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
   });
+
+
+  function scrapeProductData(productLink) {
+    fetch(proxyUrl + targetUrl + productLink)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok: ' + response.statusText);
+        }
+        return response.text();
+      })
+      .then(html => {
+
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+  
+        const articleElement = doc.querySelector('#product-article .h6.fw-bold'); 
+        const article = articleElement ? articleElement.innerText.trim() : 'Article not found';
+  
+        // console.log('Product Article:', article);
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
+  }
+  
+  
