ea439bf1ce88cd9d25809f526832d9a5f1a65a33
Mihailova Tatiana <tatiana.mihailova@isa.utm.md>
2024-10-11 12:47:35 +0300

    Extract price and name

--- request.js
+++ request.js
@@ -1,4 +1,4 @@
-const targetUrl = 'https://www.smart.md/';
+const targetUrl = 'https://www.salamander.md/ro';
 const proxyUrl = 'http://localhost:8080/';
 
 fetch(proxyUrl + targetUrl)
@@ -9,7 +9,33 @@ fetch(proxyUrl + targetUrl)
     return response.text();
   })
   .then(html => {
-    console.log(html);
+   
+    // console.log(html);
+
+    const parser = new DOMParser();
+
+    const doc = parser.parseFromString(html, 'text/html');
+
+    const products = [];
+
+    const productElements = doc.querySelectorAll('.product-card'); 
+
+    // console.log('Amount of found products:', productElements.length);
+
+    productElements.forEach(element => {
+     
+      const nameElement = element.querySelector('.product-title');
+      const name = nameElement ? nameElement.innerText.trim() : 'Name doesnt exist'; 
+
+      const priceElement = element.querySelector('.text-accent');
+      const price = priceElement ? priceElement.innerText.trim() : 'Price doesnt exist'; 
+
+      if (name && price) {
+        products.push({ name, price });
+      }
+    });
+
+    // console.log(products);
   })
   .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
